62bbf58c3d573c8863cc215ec96cc62e2eeee5eb
MYZT3RY <d1maz.d@yandex.ru>
2019-10-02 14:26:41 +0300

    3rd practic, 4th task

--- dorrer/3pr/dorrer2.21.c
+++ dorrer/3pr/dorrer2.21.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
 int main(){
-	int valueOfValues,summaryOfValues,tmp;
+	int valueOfValues,summaryOfValues,tmp,maxValue,minValue;
 	float average;
 	printf("введите количество чисел\n");
 	scanf("%i",&valueOfValues);
 	for(int i=0; i<valueOfValues; i++){
 		printf("введите число #%i\n",i+1);
 		scanf("%i",&tmp);
+		if(tmp > maxValue){
+			maxValue=tmp;
+		}
+		if(tmp < minValue){
+			minValue=tmp;
+		}
 		summaryOfValues+=tmp;
 	}
-	average=summaryOfValues/valueOfValues;
-	printf("сумма чисел - %i; среднее - %f\n",summaryOfValues,average);
+	printf("числа %i(max) и %i(min) удалены\n",maxValue,minValue);
+	average=(summaryOfValues-minValue-maxValue)/(valueOfValues-2);
+	printf("сумма чисел - %i; среднее - %.01f\n",summaryOfValues-minValue-maxValue,average);
 }
