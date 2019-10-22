2587b1c0e7d2bf27302add98d0c0ceffa1c9f0b5
MYZT3RY <d1maz.d@yandex.ru>
2019-10-22 15:27:00 +0300

    3 practic, 1st task - fix

--- dorrer/3pr/dorrer1.c
+++ dorrer/3pr/dorrer1.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 int main(){
-	int k,tmp=-1,max,first;
+	int k,tmp,max,nr,i=0;
 	printf("введите число k: ");
 	scanf("%i",&k);
 	printf("введите набор чисел\n");
-	while(tmp!=0){
+	do{
 		scanf("%i",&tmp);
-		if(first == 0){
-			first=tmp;
-		}
 		if(tmp > max){
 			max=tmp;
+			nr=i+1;
 		}
-	}
+		i++;
+	}while(tmp != 0);
 	if(max > k){
-		printf("первое число в наборе - %i\n",first);
+		printf("первое число в наборе > k - %i\n",nr);
 	}
 	else{
 		printf("не найдено\n");
