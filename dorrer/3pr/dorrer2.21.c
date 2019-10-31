0dfa3faa59d1b5b81a03cb626a1e651042d57716
MYZT3RY <d1maz.d@yandex.ru>
2019-10-31 13:24:02 +0300

    practic 3, task 2

--- dorrer/3pr/dorrer2.21.c
+++ dorrer/3pr/dorrer2.21.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	int valueOfValues,summaryOfValues,tmp,maxValue,minValue;
+	int valueOfValues,summaryOfValues,tmp,maxValue,minValue,maxRepeats=1,minRepeats=1;
 	float average;
 	printf("введите количество чисел\n");
 	scanf("%i",&valueOfValues);
@@ -9,14 +9,27 @@ int main(){
 		printf("введите число #%i\n",i+1);
 		scanf("%i",&tmp);
 		if(tmp > maxValue){
+			if(tmp > maxValue && maxRepeats != 1){
+				maxRepeats=1;
+			}
+			else{
+				maxRepeats++;
+			}
 			maxValue=tmp;
 		}
 		if(tmp < minValue){
+			if(tmp < minValue && minRepeats != 1){
+				minRepeats=1;
+			}
+			else{
+				minRepeats++;
+			}
 			minValue=tmp;
 		}
 		summaryOfValues+=tmp;
 	}
+	printf("minR - %i ; maxR - %i\n",minRepeats,maxRepeats);
 	printf("числа %i(max) и %i(min) удалены\n",maxValue,minValue);
-	average=(summaryOfValues-minValue-maxValue)/(valueOfValues-2);
-	printf("сумма чисел - %i; среднее - %.01f\n",summaryOfValues-minValue-maxValue,average);
+	average=(summaryOfValues-(minValue*minRepeats)-(maxValue*maxRepeats))/(valueOfValues-(1*maxRepeats)-(1*maxRepeats));
+	printf("сумма чисел - %i; среднее - %.01f\n",summaryOfValues-(minValue*minRepeats)-(maxValue*maxRepeats),average);
 }
